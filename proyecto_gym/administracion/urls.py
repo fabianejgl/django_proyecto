348752328a516760d21ce95e7d4eb783c7b1ef77
Fabian Gomez <fabiangl2011@gmail.com>
2023-04-20 19:49:47 -0300

    App administración renderizada. Falta 'unir' las páginas.

--- proyecto_gym/administracion/urls.py
+++ proyecto_gym/administracion/urls.py
@@ -3,5 +3,10 @@ from . import views  #me traigo todo lo de views.py
 
 urlpatterns = [
     # ejemplo clase:
-    path("administracion/", views.index_admin, name="index")
+    path("administracion/", views.index_admin, name="index"),
+    path("administracion/tables/", views.tables_admin, name="tables"),
+    path("administracion/charts/", views.charts_admin, name="charts"),
+    path("administracion/login/", views.login_admin, name="login"),
+    path("administracion/register/", views.register_admin, name="register"),
+    path("administracion/password/", views.password_admin, name="password"),
 ]
--- proyecto_gym/administracion/views.py
+++ proyecto_gym/administracion/views.py
@@ -3,5 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index_admin(request):
-    variables = 'test variable'
     return render(request, 'administracion/index_admin.html')
+
+def tables_admin(request):
+    return render(request, 'administracion/tables_admin.html')
+
+def charts_admin(request):
+    return render(request, 'administracion/charts_admin.html')
+
+def login_admin(request):
+    return render(request, 'administracion/login_admin.html')
+
+def register_admin(request):
+    return render(request, 'administracion/register_admin.html')
+
+def password_admin(request):
+    return render(request, 'administracion/password_admin.html')
