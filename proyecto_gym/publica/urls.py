4124548de0606bb55e295657d7cb7fd2328c8024
EvanaSabatella1989 <eva.sabatella@gmail.com>
2023-04-25 13:20:24 -0300

    esqueleto proyecto

--- proyecto_gym/publica/urls.py
+++ proyecto_gym/publica/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     # ejemplo clase:
     path('hola_mundo', views.hola_mundo, name ='hola'),
     path('saludar/<str:nombre>/', views.saludar, name='saludar'),
-    path("", views.index, name="index")
+    path("", views.index, name="inicio"),
+    path('quienes_somos/',views.quienes_somos,name='quienes_somos'),
+    path('proyectos/',views.proyectos,name="proyectos"),
+    path('api_proyectos/',views.api_proyectos,name="api_proyectos"),
+    path('cursos/',views.ver_cursos,name="cursos"),
 ]
--- proyecto_gym/publica/views.py
+++ proyecto_gym/publica/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
 
 
 # Create your views here.
 
 #-----Ejemplos:-----
-# def hola_mundo(request):
-#     return HttpResponse('Hola mundo Django :B')
+def hola_mundo(request):
+    return HttpResponse('Hola mundo Django :B')
 
-# def saludar(request,nombre):
-#         return HttpResponse(f"""
-#             <h1>Hola {nombre}</h1>
-#             <p>Estoy haciendo un ejemplo </p>
-#         """)
+def saludar(request,nombre):
+        return HttpResponse(f"""
+            <h1>Hola {nombre}</h1>
+            <p>Estoy haciendo un ejemplo </p>
+        """)
 
 # def index(request):
 #         if(request.method=='GET'):
@@ -24,6 +25,99 @@ from django.http import HttpResponse
 #             <h1></h1>
 #             <p>{titulo}</p>
 #         """)
+
+def index(request):    
+    mensaje=None
+    # if(request.method=='POST'):
+    #     contacto_form = ContactoForm(request.POST)
+    #     mensaje='Hemos recibido tus datos'
+    #     # acción para tomar los datos del formulario
+    # else:
+    #     contacto_form = ContactoForm()
+    listado_cursos = [
+        {
+            'nombre':'Fullstack Java',
+            'descripcion':'Curso de Fullstack',
+            'categoria':'Programación',
+        },
+        {
+            'nombre':'Diseño UX/UI',
+            'descripcion':'🖌🎨',
+            'categoria':'Diseño',
+        },
+        {
+            'nombre':'Big Data',
+            'descripcion':'test',
+            'categoria':'Análisis de Datos',
+        },
+        {
+            'nombre':'Big Data Avanzado',
+            'descripcion':'test',
+            'categoria':'Análisis de Datos',
+        },
+    ]
+
+    context = {                
+                'cursos':listado_cursos,
+                'mensaje':mensaje,
+                # 'contacto_form':contacto_form
+            }
+    return render(request,'publica/index.html',context)
+
+def quienes_somos(request):
+    template = loader.get_template('publica/quienes_somos.html')
+    context = {'titulo':'Codo A Codo - Quienes Somos'}
+    return HttpResponse(template.render(context,request))
+
+def ver_cursos(request):
+    listado_cursos = [
+        {
+            'nombre':'Fullstack Java',
+            'descripcion':'Curso de Fullstack',
+            'categoria':'Programación',             
+        },
+        {
+            'nombre':'Diseño UX/UI',
+            'descripcion':'🖌🎨',
+            'categoria':'Diseño',
+        },
+        {
+            'nombre':'Big Data',
+            'descripcion':'test',
+            'categoria':'Análisis de Datos',
+        },
+        {
+            'nombre':'Big Data Avanzado',
+            'descripcion':'test',
+            'categoria':'Análisis de Datos',
+        },
+    ]
+    return render(request,'publica/cursos.html',{'cursos':listado_cursos})
+
+def api_proyectos(request,):
+    proyectos = [{
+        'autor': 'Gustavo Villegas',
+        'portada': 'https://agenciadeaprendizaje.bue.edu.ar/wp-content/uploads/2021/12/Gustavo-Martin-Villegas-300x170.png',
+        'url':'https://marvi-artarg.web.app/'
+    },{
+        'autor': 'Enzo Martín Zotti',
+        'portada': 'https://agenciadeaprendizaje.bue.edu.ar/wp-content/uploads/2022/01/Enzo-Martin-Zotti-300x170.jpg',
+        'url':'https://hablaconmigo.com.ar/'
+    },{
+        'autor': 'María Echevarría',
+        'portada': 'https://agenciadeaprendizaje.bue.edu.ar/wp-content/uploads/2022/01/Maria-Echevarria-300x170.jpg',
+        'url':'https://compassionate-colden-089e8a.netlify.app/'
+    },]
+    response = {'status':'Ok','code':200,'message':'Listado de proyectos','data':proyectos}
+    return JsonResponse(response,safe=False)
+
+def proyectos(request):    
+    return render(request,'publica/proyectos.html')
+
+#NO USAR
+def hola_mundo(request):
+    return HttpResponse('Hola Mundo Django 🦄')
+
 
 #-----fin_ejemplos-----
 
